370ce7e6ad56a47c65768a3f1ab4a4901c26dcd0
Attreya Bhatt <attreya01@gmail.com>
2024-10-31 08:49:45 +0530

    Added URL downloader, custom django command to pull vendorfiles and docker files changes to add env variables at build time

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG",cast=bool)
-print(DEBUG)
-print(type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://xyz.prod.railway.app
@@ -50,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my-apps
+    'commando',
     'visits',
 ]
 
@@ -95,7 +94,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE",cast=int,default=30)
-DATABASE_URL = config("DATABASE_URL",cast=str)
+DATABASE_URL = config("DATABASE_URL",default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
@@ -143,11 +142,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
 # sources for collectstatic
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = [    
     STATICFILES_BASE_DIR
 ]
 
