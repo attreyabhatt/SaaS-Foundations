a03cd368d81a9d21ab7a577d74325724fb88721f
Attreya Bhatt <attreya01@gmail.com>
2024-11-05 14:14:47 +0530

    Added Subscriptions

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,15 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
+from django.conf import settings
+from django.db.models.signals import post_save
+
+User = settings.AUTH_USER_MODEL #"auth.User"
+ALLOW_CUSTOM_GROUPS = True
+
+SUBSCRIPTION_PERMISSIONS = [
+    ("advanced", "Advanced Perm"), # subscriptions.advanced
+    ("pro", "Pro Perm"),  # subscriptions.pro
+    ("basic", "Basic Perm"),  # subscriptions.basic,
+    ("ai", "AI Perm")
+]
 
 # Create your models here.
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group)
+    permissions =  models.ManyToManyField(Permission, limit_choices_to={
+        "content_type__app_label": "subscriptions", "codename__in": [x[0]for x in SUBSCRIPTION_PERMISSIONS]
+        }
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+    
 
     class Meta:
-        permissions = [
+        permissions = SUBSCRIPTION_PERMISSIONS
 
-            # to access them subscription.advanced
-            ("advanced", "Advanced Perm"), 
-            ("pro", "Pro Perm"), 
-            ("basic", "Basic Perm"),
-            ("ai", "AI Perm"),
-        ]
+
+class UserSubscription(models.Model):
+    # cascade - when the user is deleted their Subscriptions (UserSubscriptions) are deleted as well
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # a user doesn't need to have a subscription 
+    subscription = models.ForeignKey(Subscription, 
+    on_delete=models.SET_NULL, null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list('id', flat=True)
+    if not ALLOW_CUSTOM_GROUPS:
+        user.groups.set(groups_ids)
+    else:
+        subs_qs = Subscription.objects.filter(active=True)
+        if subscription_obj is not None:
+            subs_qs = subs_qs.exclude(id=subscription_obj.id)
+        subs_groups = subs_qs.values_list("groups__id", flat=True)
+        subs_groups_set = set(subs_groups)
+        # groups_ids = groups.values_list('id', flat=True) # [1, 2, 3] 
+        current_groups = user.groups.all().values_list('id', flat=True)
+        groups_ids_set = set(groups_ids)
+        current_groups_set = set(current_groups) - subs_groups_set
+        final_group_ids = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_group_ids)
+
+
+post_save.connect(user_sub_post_save, sender=UserSubscription)
