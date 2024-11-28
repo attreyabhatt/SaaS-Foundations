9be55f188fe7a69b181c39216e6a5859189287b2
Attreya Bhatt <attreya01@gmail.com>
2024-11-28 11:03:34 +0530

    Hello world gitlab action

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from .models import SubscriptionPrice
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from subscriptions.models import SubscriptionPrice, UserSubscription
+
+@login_required
+def user_subscription_view(request,):
+    user_sub_obj, created = UserSubscription.objects.get_or_create(user=request.user)
+    if request.method == "POST":
+        print("refresh sub")
+        finished = subs_utils.refresh_active_users_subscriptions(user_ids=[request.user.id], active_only=False)
+        if finished:
+            messages.success(request, "Your plan details have been refreshed.")
+        else:
+            messages.error(request, "Your plan details have not been refreshed, please try again.")
+        return redirect(user_sub_obj.get_absolute_url())
+    return render(request, 'subscriptions/user_detail_view.html', {"subscription": user_sub_obj})
+
 
 # Create your views here.
 def subscription_price_view(request,interval="month"):
